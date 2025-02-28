bd609ed9bc7c4489c05791a33afe9e4b07ed6721
mokoto-k <mokotok@protonmail.com>
2025-02-28 21:21:43 +1000

    computer science server rebuild

---
+++ 09-data structures and algorithms/week-1/Rational.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <ostream>
+
+class Rational {
+friend std::ostream& operator<<(std::ostream&, const Rational&);
+public:
+    Rational(): numerator(), denominator(1) {}
+    Rational(int, int);
+    void reduce();
+    Rational operator+(const Rational&) const;
+private:
+    int numerator;
+    int denominator;
+};
+
