476676a2148b77a1aba15f1372142b3315d84548
mokoto-k <mokotok@protonmail.com>
2025-03-01 11:35:47 +1000

    overloading operators in c++

---
+++ 09-data structures and algorithms/week-1/rational/Rational.h
@@ -0,0 +1,16 @@
+#include <iostream>
+
+class Rational {
+    friend std::ostream& operator<<(std::ostream&, const Rational&);
+
+public:
+    Rational(): numerator(), denominator(1) {}
+    Rational(int, int);
+    void reduce();
+    Rational operator+(const Rational&) const;
+
+private:
+    int numerator;
+    int denominator;
+};
+
