746a00e6c2a4c75f76abd81d64e658469840b50c
mokoto-k <mokotok@protonmail.com>
2025-03-02 19:44:19 +1000

    chp5 database exercises

---
+++ 09-data structures and algorithms/week-1/Bank_account.h
@@ -0,0 +1,18 @@
+// The class interface
+#include <string>
+
+class Bank_account {
+
+public:
+    Bank_account(): account_number(), balance() {}
+    Bank_account(std::string, int, double);
+    void set_balance(double);
+    std::string get_customer_name() const;
+    int get_account_number() const;
+    double get_balance() const;
+
+private:
+    std::string customer_name;
+    int account_number;
+    double balance;
+};
