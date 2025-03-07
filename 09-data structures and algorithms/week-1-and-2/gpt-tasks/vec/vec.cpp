44e9df6cc170d096f0f150bb0d83cd9c54ff5904
mokoto-k <mokotok@protonmail.com>
2025-03-07 11:11:30 +1000

    ass1 started

--- 09-data structures and algorithms/week-1-and-2/gpt-tasks/vec/vec.cpp
+++
@@ -1,9 +0,0 @@
-#include "vec.h"
-#include <iostream>
-
-Vec::Vec() : array_limit(0), pointer_to_data(nullptr) {}
-
-Vec::Vec(int a) : array_limit(a), pointer_to_data(new double[a]) {
-    for (int i = 0; i < arracy_limit; ++i)
-        pointer_to_data[i] = 0.0;
-}
